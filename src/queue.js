a170439b5050f5b47ca4ad84b1e4891cf513c406
Irina Fileva <irinkaskai@gmail.com>
2023-12-21 14:11:48 +0500

    feat: do the tack queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+ const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,21 +14,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+   constructor(){
+   }
+
+  head = null;
+  tail = null;
+   
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.head;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    let queue = new ListNode(value)
+    if(!this.tail){
+      this.head = queue
+      this.tail = queue;
+    }
+    else{
+      this.tail.next = queue;
+      this.tail = queue;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(!this.head){
+      return null;
+    }
+    
+    let val = this.head.value
+
+    if(this.head.next){
+      this.head = this.head.next
+    }
+    else{
+      this.head = null;
+      this.tail = null;
+    }
+    return val
   }
+
 }
 
 module.exports = {
